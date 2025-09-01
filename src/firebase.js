cc99336713c1ae35e13654a6777ae7331ed683b3
👨‍💻 Dotun Front-End Developer Portfolio <youronlydotuncrypt@gmail.com>
2025-09-01 04:33:13 +0100

    add base project structure with index and firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,1 @@
-// Firebase configuration (dummy placeholder)
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-};
-
-// Normally you’d initialize Firebase here
-// Example:
-// import { initializeApp } from "firebase/app";
-// const app = initializeApp(firebaseConfig);
-
-console.log("Firebase config loaded:", firebaseConfig);
+// Firebase config will go here later
